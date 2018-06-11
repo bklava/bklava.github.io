0a0a591a38d91cbbc10ff41a0c794bb48db12bfc
Bruno Klava <bruno@tvod.com.br>
2018-06-11 13:51:46 -0300

    remvoign aldeia code

--- aldeia/src/Messages.ts
+++
@@ -1,8 +0,0 @@
-const Messages = {
-    withoutUserMediaAPI: 'Navegador sem suporte a acessar câmera',
-    qrCodeLabel: 'Código QR',
-    notDetected: 'Não detectado',
-    lastCodeDetectedLabel: 'Último código detectado: '
-}
-
-export default Messages;
